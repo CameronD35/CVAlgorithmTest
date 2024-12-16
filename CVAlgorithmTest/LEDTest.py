199cf76bd01369520c7c2d5331649acab713e622
CameronD35 <camster09@ca.rr.com>
2024-12-16 14:12:12 -0800

    Added comments for clarity. Commented out unecessary code.

--- CVAlgorithmTest/LEDTest.py
+++ CVAlgorithmTest/LEDTest.py
@@ -1,14 +1,19 @@
 import cv2 as cv
 import numpy as np
 
+# Reads in the image
 img = cv.imread('purpleLED.jpg')
-height, width = img.shape[:2]
 
-blank = np.zeros(img.shape[:2], dtype='uint8')
+# Splits the image into the three different color channels
+# ---- IRRELAVENT ----
 
-blue, green, red = cv.split(img)
-blueImg = cv.merge([blue, blank, blank])
-redImg = cv.merge([blank, blank, red])
+# height, width = img.shape[:2]
+
+# blank = np.zeros(img.shape[:2], dtype='uint8')
+
+# blue, green, red = cv.split(img)
+# blueImg = cv.merge([blue, blank, blank])
+# redImg = cv.merge([blank, blank, red])
 
 # lowerBlue = np.array([50, 0, 0], dtype='uint8')
 # upperBlue = np.array([255, 255, 255], dtype='uint8')
@@ -16,22 +21,33 @@ redImg = cv.merge([blank, blank, red])
 # colorMask = cv.inRange(img, lowerBlue, upperBlue)
 #detectedBlue = cv.bitwise_and(img, img, mask=colorMask)
 
+# ---- RELEVANT ----
+
+# Sets the image to grayscasle
 grayscaleImg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-
+# Thresholds the image to intensities 247 and above
+# Lower intensity will make the circles cover more of the LED, but the center will likely remain the same
+# The center is key for tracking where the LED actually is
 grayThreshold, grayThresholdedImg = cv.threshold(grayscaleImg, 247, 255, cv.THRESH_BINARY)
 
+# Adds a slight blur to compensate for gaps in the thresholding process
 grayThreshold_Blurred = cv.blur(grayThresholdedImg, (3,3))
 
+# Uses the canny algorithm to find edges in the thresholded images
+# Essentially, this outlines the LEDs
 edges = cv.Canny(grayThreshold_Blurred, 0, 255)
 
+# Gets the coordinates of various parts of the edges
 contours, hierarchies = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
 
-# [max, min]
+# In the format [max, min]
 xCoordinatesArray = []
 yCoordinatesArray = []
 
-
+# This loops through the contour coordinates found in the 'contours' variable
+# Note how the contours is a 3D array
+# This code also finds the max and min of the coordinates since between those points houses most of the contour (LED outline)
 i = 1
 for contour in contours:
     # print(f'\n\n-------- Contour {i} --------\n\n')
@@ -52,12 +68,15 @@ for contour in contours:
     yCoordinatesArray.append([np.min(yArray), np.max(yArray)])
     i += 1
 
-print('x min and max:', xCoordinatesArray)
-print('y min and max:', yCoordinatesArray)
+# print('x min and max:', xCoordinatesArray)
+# print('y min and max:', yCoordinatesArray)
 
+# Converts the arrays to Numpy format since OpenCV methods require this format
 xCoordinatesArray = np.array(xCoordinatesArray, dtype=np.uint32)
 yCoordinatesArray = np.array(yCoordinatesArray, dtype=np.uint32)
 
+# Loops through the arrays of coordinates, calculates their centers, and the radii the ellipse would have to be to include the extrema
+# The program then circles where the light is detected, provides its center and prints text to indicate the different LEDs
 for i in range (0, len(xCoordinatesArray)):
 
     # [min. max]
@@ -92,6 +111,9 @@ for i in range (0, len(xCoordinatesArray)):
 
 # print(len(contours))
 
+
+# YOU CAN IGNORE ALL THIS. This was used by a different algorithm that detects circlesw.
+# However, the glare from LEDs is rarely ever a perfect or near-perfect circle
 
 
 # detectedCircles = cv.HoughCircles(grayThreshold_Blurred, cv.HOUGH_GRADIENT, 1, 20, param1 = 50, param2 = 30, minRadius = 1, maxRadius = 40)
